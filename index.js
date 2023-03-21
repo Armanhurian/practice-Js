ac04eb5eace5a392be0c6e3d6e5116663a763b7a
Arman <hurianarmann@gmail.com>
2023-03-21 22:43:05 +0330

    Add Day 4

--- index.js
+++ index.js
@@ -222,3 +222,101 @@ sumNumber(2,4,6,54,32,99)
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//start set number and sum for example : 194 => 1+9+4 = 14
+
+// let inputNumber = Number( prompt('عدد رو وارد کنید'))
+// let sumNumberDigit = 0
+
+// if(inputNumber === 0 ){
+//     alert('please set your input')
+    
+// }else if(isNaN(inputNumber)=== true){
+//     alert('not a number')
+// }else if(isNaN(inputNumber) === false){
+    
+//     inputNumber = String(inputNumber)
+    
+//     for (let i = 0 ; i < inputNumber.length; i++) {
+//         let myStringNumber = inputNumber[i];
+//         myStringNumber = Number(myStringNumber)
+//         sumNumberDigit += myStringNumber
+//     }
+//     console.log(sumNumberDigit);  
+
+// }
+
+//finish set number and sum for example : 194 => 1+9+4 = 14
+
+//start random quote
+
+let submitForTextBtn = document.querySelector('.submitForTextBtn')
+let subTitleTextRandom = document.querySelector('.subTitleTextRandom')
+
+let famousPeopleWordsArray = [
+    {name : 'کریس ماسگرو' , text : 'موفقیت در این نیست که چه چیزی در پیش رو داریم، موفقیت در این است که چه چیزی در پشت سر به جا می‌گذاریم.'},
+    {name : ' اندرو هندریکسون' , text : 'هر کسی که توانسته است چیزی را بسازد، حتما نظم و انضباط داشته است.'},
+    {name : 'اسکار وایلد' , text : 'آنچه ما امتحان‌هایی سخت می‌پنداریم‌شان غالبا موهبت‌هایی با لباس مبدل‌اند.'},
+    {name : 'کریس گروسر' , text : 'فرصت‌ها اتفاق نمی‌افتند، شما خالق آنها هستید.'},
+    {name : 'وینستون چرچیل' , text : 'موفقیت عبارت است از رفتن از شکست به شکست دیگر بدون از دست دادن اشتیاق...'},
+    {name : 'وینسنت ون ‌گوگ' , text : 'رؤیاهای‌ خودتان را بسازید در غیر این صورت فرد دیگری شما را برای ساختن رؤیایش به کار خواهد گرفت..'},
+]
+
+
+
+let randomIndex = () => {
+    let createRandomIndex = Math.floor((Math.random()*famousPeopleWordsArray.length))
+    console.log(createRandomIndex);
+    let getRandomTextFromArray = famousPeopleWordsArray[createRandomIndex];
+    subTitleTextRandom.innerHTML = getRandomTextFromArray.text
+
+}
+submitForTextBtn.addEventListener('click',() => {
+
+    randomIndex()
+
+})
+
+//finish random quote
+
+//start create table for develpment task 
+
+let tableTasks = document.querySelectorAll('.tableTasks')
+let selectOption = document.querySelector('.selectOption')
+let setDeveloperTaskInput = document.querySelector('.setDeveloperTaskInput')
+let tableTbodyTasks = document.querySelector('.tableTbodyTasks')
+let btnSubmitTask = document.querySelector('.btnSubmitTask')
+let tdOne = document.querySelector('.tdOne')
+let tdTwo = document.querySelector('.tdTwo')
+let tdThree = document.querySelector('.tdThree')
+
+
+
+let createAndAppendTasks = () => {
+       let tableTask = document.createElement('tr')
+       tableTask.classList.add('tableTasks')
+       let tableTDOne = document.createElement('td')
+       let tableTDTwo = document.createElement('td')
+       let tableTDThree = document.createElement('td')
+       tableTask.append(tableTDOne)
+       tableTask.append(tableTDTwo)
+       tableTask.append(tableTDThree)
+       tableTbodyTasks.append(tableTask)
+       console.log(tableTbodyTasks);
+    
+    if(tdOne.dataset.key === selectOption.value){
+           tableTDOne.innerHTML = setDeveloperTaskInput.value
+       }else if(tdTwo.dataset.key === selectOption.value){
+           tableTDTwo.innerHTML = setDeveloperTaskInput.value
+       }else{
+           tableTDThree.innerHTML = setDeveloperTaskInput.value
+
+    }
+    
+}
+
+btnSubmitTask.addEventListener('click',()=>{
+    createAndAppendTasks()
+})
+
+//finish create table for develpment task 
